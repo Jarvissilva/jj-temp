f9fc4286111539fa0ab79d6d3e0c938a4913bcf9
Joselito <joselitorebello20@gmail.com>
2026-03-03 11:05:03 +0530

    fix video playback issues

--- src/components/Bhideo.js
+++ src/components/Bhideo.js
@@ -8,7 +8,7 @@ const Bhideo = () => {
     <>
     <div className="w-[100vw] h-[100vh] lg:w-[95vw] lg:h-[95vw] xl:h-[90vh] 2xl:h-[95vh] 2xl:w-[70vw] flex items-center justify-center  bg-black rounded-2xl">
 
-     <Video  autoplay src={getStarted} poster={gift} />
+     <Video  autoPlay src={getStarted} poster={gift} />
     </div>
     </>
   );
