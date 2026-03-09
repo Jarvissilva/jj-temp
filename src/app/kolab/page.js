79cec094054a1cd851d0866a30cf0dea9eb2f4a6
Jarvissilva <jarvissilva212@gmail.com>
2026-03-09 15:15:43 +0530

    changed design of editor

--- src/app/kolab/page.js
+++ src/app/kolab/page.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function page() {
   return (
-    <div>Kollabration tenkai</div>
-  )
+    <main className="h-screen">
+      <div>Kollabration tenkai</div>
+      <div className="p-4 w-full h-full">
+        <textarea className="border shadow-2xl border-gray-200 p-4 w-full h-full bg-gray-500"></textarea>
+      </div>
+    </main>
+  );
 }
 
-export default page
+export default page;
